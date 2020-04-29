18531dc0673dffab93ee83533cd80d2b974c44d9
melandres8 <melanmosta@gmail.com>
2020-04-29 00:06:01 +0000

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-for item in range(122, 96, -1):
-    uniCode = ord(chr(item))
-    if uniCode % 2 == 0:
-        alph = chr(uniCode)
-        print(alph, end="")
-    elif uniCode % 2 == 1:
-        alph = chr(uniCode - 32)
-        print(alph, end="")
+for item in range(90, 64, -1):
+    if item % 2 == 0:
+        item += 32
+    print(chr(item), end="")
