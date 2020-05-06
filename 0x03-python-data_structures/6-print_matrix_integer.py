cfe811c30363bacf88d5b862c9a4bb060e00ab42
melandres8 <melanmosta@gmail.com>
2020-05-06 00:20:37 +0000

    checker

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for item in matrix:
-        print(" ".join(map(str, item)))
+            print(' '.join("{:d}".format(i) for i in item))
