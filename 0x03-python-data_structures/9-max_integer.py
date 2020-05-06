fbf8ce2159430ee0f1591694b0dc95c9ffa97cdb
melandres8 <melanmosta@gmail.com>
2020-05-06 01:33:12 +0000

    programacion orientada al checker

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = 0
-    if my_list:
-        for num in my_list:
-            if num > max_value:
-                max_value = num
-        return max_value
-    return None
+    if not my_list:
+        return None
+    else:
+        my_list.sort()
+        return my_list[-1]
