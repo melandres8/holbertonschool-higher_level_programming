1ba4287dd94e11e703057d8523d2a7e96e3828b0
melandres8 <melanmosta@gmail.com>
2020-05-06 02:10:39 +0000

    fixasd

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
     le = len(my_list)
     for item in range(le):
-        if item % 2 == 0:
+        if new_list[item] % 2 == 0:
             new_list[item] = True
         else:
             new_list[item] = False
