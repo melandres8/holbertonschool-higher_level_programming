492a3fd45343962f3fae2df7a0780ba966cfad4f
melandres8 <melanmosta@gmail.com>
2020-05-07 22:45:03 +0000

    *Fix*

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+def power(b, x):
+    return b ** x
+
+
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for item in matrix:
-        power = lambda x: x ** 2
-        new_matrix += [[power(x) for x in item]]
+        new_matrix += [[power(n, 2) for n in item]]
     return new_matrix
