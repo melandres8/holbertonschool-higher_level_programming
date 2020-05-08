024eb4d7b0acdeb17b69fc8d42ee64b74a3cfc34
melandres8 <melanmosta@gmail.com>
2020-05-08 00:10:45 +0000

    epa

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for item in set_1:
-        for item1 in set_2:
-            if item == item1:
-                return item
+    if set_1 & set_2: 
+        return set_2 & set_1
