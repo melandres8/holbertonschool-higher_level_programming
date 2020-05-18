f0688342e04c9a623fcb819fab6ef32f309f320e
melandres8 <melanmosta@gmail.com>
2020-05-18 20:09:10 +0000

    {Task} safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = None
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+    return result
