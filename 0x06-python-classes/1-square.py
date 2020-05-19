5ce1a99387badf88a08939cfb5f15ff8c12c7bfd
melandres8 <melanmosta@gmail.com>
2020-05-19 20:33:49 +0000

    {Requirements} adding and fix docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-class Square:
+"""Square class"""
+
+
+class Square():
+
     """__init__ always runs when we create
-    a new object of a class"""
+    a new object of a class
+    """
+
     def __init__(self, size=0):
-        """Creating a private instance"""
+        """
+        Creating a private instance
+        """
         self.__size = size
