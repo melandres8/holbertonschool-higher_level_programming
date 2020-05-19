ea8bd16665259d44471fc9e3df483d4ebdba75e3
melandres8 <melanmosta@gmail.com>
2020-05-19 20:50:53 +0000

    {docstring} adding and fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,18 +5,35 @@
 class Square():
 
     """Constructor function defined using
-    __init__"""
+    __init__
+    Attributes:
+        attr1 (int): size if a main attr of a square
+    """
     def __init__(self, size=0):
+        """
+        Inicializing my class with
+
+        Args:
+            size (int): size of a square
+        """
         self.__size = size
 
-    """Using decorator property to handler with
-    getters and setter"""
     @property
     def size(self):
+        """Getter, retrieve the size of a square
+        this methods is useful to handle with
+        privacity of our instance"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Handle the value of a size if
+        is an integer or not
+
+        Args:
+            size (int): size of a square
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -24,4 +41,5 @@ class Square():
         self.__size = value
 
     def area(self):
+        """Calculating the area of a square"""
         return int(self.__size) * int(self.__size)
