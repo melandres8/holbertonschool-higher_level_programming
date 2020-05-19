8117e8ed2bec40779f25731ca86a2078e76be194
melandres8 <melanmosta@gmail.com>
2020-05-19 20:43:56 +0000

    {docstring} adding

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,8 +4,18 @@
 
 class Square():
     """Validating if size is an instance
-    and if is greater and equal to 0"""
+    and if is greater and equal to 0
+    Attributes:
+        attr1 (int): size is a main attr of a square
+    """
     def __init__(self, size=0):
+        """isinstance function checks if
+        the object is an instance or subclass
+        of the second argument
+
+        Args:
+            size (int): size of my square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
