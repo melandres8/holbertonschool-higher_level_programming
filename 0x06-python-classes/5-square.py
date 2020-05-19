6a3033ea696e7079fb3ef12cb857402019eff2cc
melandres8 <melanmosta@gmail.com>
2020-05-19 20:53:34 +0000

    {docstring} fix and adding

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,16 +5,35 @@
 class Square():
 
     """__init__ constructor: Runs always when we
-    create a new instance of a class"""
+    create a new instance of a class
+    Attributes:
+        attr1 (int): is the size of a square
+    """
     def __init__(self, size=0):
+        """
+        Inicializing my class with
+
+        Args:
+            size (int): size of a square
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Getter, retrieve the size of a square
+        this methods is useful to handle with
+        privacity of our instance"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Handle the value of a size if
+        is an integer or not
+
+        Args:
+            size (int): size of a square
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -22,11 +41,12 @@ class Square():
         self.__size = value
 
     def area(self):
+        """Calculating the area of a square"""
         return int(self.__size) * int(self.__size)
 
-    """Printing a Square and validating
-    if the size is 0 or not"""
     def my_print(self):
+        """Printing a Square and validating
+        if the size is 0 or not"""
         for row in range(self.__size):
             print('#' * self.__size)
         if self.__size == 0:
