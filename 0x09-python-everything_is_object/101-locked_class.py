d3aef109e5dc9e02bd1a963569336cf307e78ac6
melandres8 <melanmosta@gmail.com>
2020-05-26 23:19:34 +0000

    {doc}

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-class LockedClass():
+"""Locked class"""
+
+
+class LockedClass:
     """_slots__ allow us to explicitly declare data members
     (like properties) and deny the creation of __dict__ and __weakref__
     (unless explicitly declared in __slots__ or available in a parent.)
