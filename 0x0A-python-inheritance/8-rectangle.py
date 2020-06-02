81a992deca8b3157539cfe1cb8f72acc3bab403a
melandres8 <melanmosta@gmail.com>
2020-06-02 23:26:42 +0000

    {docs} adding docs

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,7 +4,15 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ defining constructor method
+    """
     def __init__(self, width, height):
+        """ Constructor
+
+            Args:
+                width: Rectangle width
+                height: Rectangle height
+        """
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
