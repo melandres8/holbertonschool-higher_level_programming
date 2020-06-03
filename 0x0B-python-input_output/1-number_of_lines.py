7bbde9ad7c43a28a66728d05b2a406903b3f831b
melandres8 <melanmosta@gmail.com>
2020-06-03 23:01:34 +0000

    {fix} docs

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,6 +5,9 @@ in a given file
 
 
 def number_of_lines(filename=""):
+    """ Returning the number of line
+        in a text file
+    """
     with open(filename, 'r') as file:
         n_lines = 0
         for line in file:
