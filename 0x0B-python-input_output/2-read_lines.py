9d4867655839944c413233e9de0d55aa35c8edaf
melandres8 <melanmosta@gmail.com>
2020-06-03 23:06:18 +0000

    {fix} output

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,7 +7,7 @@ def read_lines(filename="", nb_lines=0):
     """ Printing n lines of a text file
     """
     with open(filename, 'r') as file:
-        if 0 > nb_lines < len(open(filename).readlines()):
+        if 0 < nb_lines < len(open(filename).readlines()):
             while nb_lines:
                 print(file.readline(), end='')
                 nb_lines -= 1
