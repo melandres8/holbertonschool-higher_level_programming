4c4a9ae8b31ac0dd5f4202371e816ef7b24a2001
melandres8 <melanmosta@gmail.com>
2020-06-04 01:44:58 +0000

    {task#13}

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student to JSON
+"""
+
+
+class Student():
+    """ Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Constructor method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dict representation
+            of a Student instance
+        """
+        dic = dict()
+        if type(attrs) == list:
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    dic[k] = v
+        else:
+            return self.__dict__
+        return dic
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
