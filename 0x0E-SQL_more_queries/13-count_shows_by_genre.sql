8fcb7a1594a575a0b11a1a1890e8399642e9d9ae
melandres8 <melanmosta@gmail.com>
2020-07-15 02:54:34 +0000

    issue: damn it!

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name AS genre,
 COUNT(*) AS number_of_shows
 FROM tv_show_genres JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY genres
+GROUP BY genre
 ORDER BY number_of_shows DESC;
 
