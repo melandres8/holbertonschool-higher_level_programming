0ac36132599628fb0e0064c4b76256a26dd449e3
melandres8 <melanmosta@gmail.com>
2020-07-15 02:18:25 +0000

    issue: number_shows as number_of_shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_shows
+SELECT tv_genres.name as genre,
+COUNT(tv_show_genres.genre_id) as number_of_shows
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
