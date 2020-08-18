da8e779afcf53333700b73a671df93febf4c6136
melandres8 <melanmosta@gmail.com>
2020-08-18 23:55:42 +0000

    80megas

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,11 +6,7 @@ def find_peak(list_of_integers):
     """
     This function returns the peak of a list
     """
-    if list_of_integers:
-        if(list_of_integers[0] > list_of_integers[1]):
-            return list_of_integers[0]
-        elif(list_of_integers[-1] > list_of_integers[-2]):
-            return list_of_integers[-1]
-        else:
-            return(max(list_of_integers))
-    return(None)
+    if list_of_integers == []:
+        return None
+    s = sorted(list_of_integers)
+    return s[-1]
