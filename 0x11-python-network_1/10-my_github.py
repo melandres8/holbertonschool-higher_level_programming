b30014227a778f592d1294775f38f7bf73636e72
melandres8 <melanmosta@gmail.com>
2020-08-25 02:07:30 +0000

    task#10: my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My github """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    url = 'https://api.github.com/user'
+
+    req = requests.get(url, auth=(username, password))
+    print(req.json().get('id'))
