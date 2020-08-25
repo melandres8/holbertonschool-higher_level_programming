b743152482505c37519c315a66d275b299770511
melandres8 <melanmosta@gmail.com>
2020-08-25 01:21:35 +0000

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     req = requests.get(url)
     if req.status_code >= 400:
-        print(req.status_code)
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
