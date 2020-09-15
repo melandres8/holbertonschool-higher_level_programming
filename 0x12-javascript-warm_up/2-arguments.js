528c39a447e17b01091da76c0212c6d97eb4915f
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-15 14:22:16 -0500

    task#2: arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 // script that prints a message depending of the number of arguments passed
-if (process.argv.length < 3) {
-  console.log('Not argument');
-} else if (process.argv.length === 3) {
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log('No argument');
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
