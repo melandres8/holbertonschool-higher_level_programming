29da7630b6ea0587b14b8882294c6129bbe0048f
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-15 15:16:33 -0500

    task#6: loop to language

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// multi-lines using loops
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
