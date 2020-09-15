27766a9f06351cd1129119646ec1c212fb697f08
Melkin Mosquera <melanmosta@gmail.com>
2020-09-15 16:21:14 -0500

    task#10: factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/local/bin/node
+// script that computes and prints a factorial
+const myNumber = parseInt(process.argv[2]);
+
+function Factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * Factorial(n - 1);
+  }
+}
+
+console.log(Factorial(myNumber));
