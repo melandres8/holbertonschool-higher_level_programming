47452047c0a14a3798df6836a63720803eda01dc
Melkin Mosquera <melanmosta@gmail.com>
2020-09-15 16:12:20 -0500

    task#9: add func

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints the addition of 2 integers
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(arg1, arg2));
