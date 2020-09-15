42921e35313a1703ebc030a0722ac456cda7d9bd
Melkin Mosquera <melkinmosquera@Melkins-MacBook-Air.local>
2020-09-15 14:17:13 -0500

    task#2: arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/node
+// script that prints a message depending of the number of arguments passed
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log('Not argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
